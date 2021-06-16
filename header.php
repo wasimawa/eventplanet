35624d56f730cb59ff38bb571349027138b88f77
Ayesha Tabassum A <ayesha.tab6@gmail.com>
2021-06-16 21:03:39 +0530

    my first commit using git

---
+++ header.php
@@ -0,0 +1,86 @@
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Wedding Organizer Wedding Category Flat Bootstrap Responsive Website Template | Home : W3layouts</title>
+
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Wedding Organizer web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript">
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!--// Meta tag Keywords -->
+
+    <!-- css files -->
+    <link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css"> <!-- Font-Awesome-Icons-CSS -->
+    <!-- //css files -->
+
+    <!--web font-->
+    <link href="//fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+    <!--//web font-->
+
+</head>
+<body>
+
+
+<!-- header -->
+<header>
+    <div class="container">
+        <!-- top header -->
+        <section class="row top_header pt-3">
+            <div class="col-lg-6 buttons ml-auto">
+                <p><span class="fa fa-phone"></span> +12 8976 2334</p>
+                <a class="btn btn-info btn-lg-block w3ls-btn px-sm-4 px-3 text-capitalize mr-sm-2" href="#login-popup">Login</a>
+                <a class="btn btn-info1 btn-lg-block w3ls-btn1 px-sm-4 px-3 text-capitalize" href="#register-popup">Register</a>
+            </div>
+        </section>
+        <!-- top header -->
+        <!-- nav -->
+        <nav class="py-3">
+            <div id="logo">
+                <h1>
+                    <a class="navbar-brand" href="index.php"> <span class="fa fa-empire"></span>Wedding <span><span class="line"></span>Organizer</span>
+                    </a>
+                </h1>
+            </div>
+
+            <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+            <input type="checkbox" id="drop" />
+            <ul class="menu mt-2">
+                <li class="mr-lg-3 mr-2 active"><a href="index.php">Home</a></li>
+                <li class="mr-lg-3 mr-2"><a href="about.php">About Us</a></li>
+                <li class="mr-lg-3 mr-2"><a href="services.php">Services</a></li>
+                <li class="mr-lg-3 mr-2"><a href="gallery.php">Gallery</a></li>
+                <li class="mr-lg-3 mr-2 pb-0">
+                    <!-- First Tier Drop Down -->
+                    <label for="drop-2" class="toggle">Pages <span class="fa fa-angle-down" aria-hidden="true"></span> </label>
+                    <a href="#">Pages <span class="fa fa-angle-down" aria-hidden="true"></span></a>
+                    <input type="checkbox" id="drop-2"/>
+                    <ul class="drop-down-ul">
+                        <li><a href="error.php">Error Page</a></li>
+                        <li><a href="events.php">Events Page</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <!-- //nav -->
+    </div>
+</header>
+<!-- //header -->
